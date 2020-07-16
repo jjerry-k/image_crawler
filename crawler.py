5f3075e1edda766cf5ebae0198e19b899a2594bf
jjerry-k <jaeyeol2931@gmail.com>
2020-07-17 00:53:10 +0900

    Add .gitignore & Edit crawler.py

--- crawler.py
+++ crawler.py
@@ -1,26 +1,50 @@
 # %%
-from bs4 import BeautifulSoup
-import urllib3
+import os, time, urllib3, platform, argparse
 from pprint import pprint
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+def crawler(keyword, dst_root, mode = "headless"):
+
+    # Check Platform 
+    if platform.system() == 'Windows':
+        print('Detected OS : Windows')
+        executable = './webdriver/chromedriver.exe'
+    elif platform.system() == 'Linux':
+        print('Detected OS : Linux')
+        executable = './webdriver/chromedriver_linux'
+    elif platform.system() == 'Darwin':
+        print('Detected OS : Mac')
+        executable = './webdriver/chromedriver_mac'
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+    options.add_argument("lang=ko_KR") # 한국어!
+    if mode == "headless":
+        options.add_argument(mode)
+    driver = webdriver.Chrome(executable, chrome_options=options)
+    driver.implicitly_wait(1.5)
+
+    driver.get(f"https://www.google.com/search?q={keyword}&source=lnms&tbm=isch")
+
+    time.sleep(1)
+
+    elem = driver.find_element_by_tag_name("body")
+
+    print(elem)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--keyword", dest="keyword", type=str, default="cat", help="Keyword that you want to collect")
+    parser.add_argument("--save_root", dest="save_root", type=str, default="./result", help="Path of save root directory")
+    parser.add_argument("--mode", dest="mode", type=str, default="noneP", help="Kind of webdriver")
+    args = parser.parse_args()
+
+    dst_path = os.path.join(args.save_root, args.keyword)
+    os.makedirs(dst_path, exist_ok=True)
+    crawler(args.keyword, dst_path, args.mode)
 
 # %%
-http = urllib3.PoolManager()
-url = 'https://www.google.com/search?q=cat&sxsrf=ALeKk00gYYCaB11TlQGVpvCbViRwsBIwIQ:1594734972464&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjtoa3c8szqAhWoGKYKHfxMDX4Q_AUoAXoECBcQAw&biw=1726&bih=1267'
-
-# %%
-response = http.request('GET', url)
-
-# print(source)
-# %%
-soup = BeautifulSoup(response.data, features="html.parser")
-
-img = soup.find("img")  # 이미지 태그
-# img_src = img.get("src") # 이미지 경로
-# img_url = base_url + img_src # 다운로드를 위해 base_url과 합침
-# img_name = img_src.replace("/", "") # 이미지 src에서 / 없애기
-# print(soup)
-print(img) 
-
-img = soup.find_all("img")
-pprint(img)
-print(len(img))
